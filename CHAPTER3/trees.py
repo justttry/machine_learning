3f2ceefb2d01499f84ec18fc9006fdf5dd97e308
justttry <kerzhao@163.com>
2016-12-12 19:26:14 +0800

    refresh chapter 2

--- CHAPTER3/trees.py
+++ CHAPTER3/trees.py
@@ -1,6 +1,8 @@
 #encoding: UTF-8
 
 from math import log
+import operator
+import matplotlib.pyplot as plt
 
 #----------------------------------------------------------------------
 def calcShannonEnt(dataSet):
@@ -28,3 +30,73 @@ def createDataSet():
                [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
     return dataSet, labels
+
+#----------------------------------------------------------------------
+def splitDataSet(dataSet, axis, value):
+    """"""
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+#----------------------------------------------------------------------
+def chooseBestFeatureToSplit(dataSet):
+    """"""
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if(infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+#----------------------------------------------------------------------
+def majorityCnt(classList):
+    """"""
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key=operator.itemgetter(1),
+                              reverse=True)
+    return sortedClassCount[0][0]
+
+#----------------------------------------------------------------------
+def createTree(dataSet, labels):
+    """"""
+    classList = [i[-1] for i in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del labels[bestFeat]
+    featValues = [i[bestFeat] for i in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet\
+                                                  (dataSet, bestFeat, value),
+                                                  subLabels)
+    return myTree
+
+decisionNode = dict(boxstyle='sawtooth', fc='0.8')
+leafNode = dict(boxstyle='round4', fc='0.8')
+arrow_args = dict(arrowstyle='<-')
